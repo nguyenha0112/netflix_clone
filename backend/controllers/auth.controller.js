c2e44e6a9e54fc120ffda1890a7eccb6121c9109
NguyenHa <nguyen229396@gmail.com>
2025-01-04 03:35:44 +0700

    add login , logout, signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { generateTokenandsetcookie } from "../utils/generateToken.js";
 
 // hàm đăng kí người dùng mới
 export async function signup(req, res) {
@@ -25,38 +27,33 @@ export async function signup(req, res) {
 
     // Kiểm tra độ dài mật khẩu phải tối thiểu 6 ký tự
     if (password.length < 6) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message: "Password must be at least 6 characters",
-        });
+      return res.status(400).json({
+        success: false,
+        message: "Password must be at least 6 characters",
+      });
     }
 
     // Kiểm tra xem email đã tồn tại trong cơ sở dữ liệu chưa
     const existingUserByEmail = await User.findOne({ email: email });
     if (existingUserByEmail) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message: "User with this email already exists",
-        });
+      return res.status(400).json({
+        success: false,
+        message: "User with this email already exists",
+      });
     }
 
     // Kiểm tra xem tên người dùng (username) đã tồn tại chưa
     const existingUserByName = await User.findOne({ username: username });
     if (existingUserByName) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message: "User with this name already exists",
-        });
+      return res.status(400).json({
+        success: false,
+        message: "User with this name already exists",
+      });
     }
 
-    const salt = await bcrypt.genSaltSync(10);
-    const hashedPassword = await bcrypt.hashSync(password, salt);
+    const salt = await bcryptjs.genSalt(10); // tạo muối cho mật khẩu
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
     // bảo mật cho mật khẩu người dùng
 
     // Danh sách hình đại diện mặc định
@@ -68,23 +65,84 @@ export async function signup(req, res) {
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       image,
     });
 
     // Lưu người dùng mới vào cơ sở dữ liệu
+    if (newUser) {
+      generateTokenandsetcookie(newUser._id, res);
+      res.status(201).json({
+        success: true,
+        user: {
+          ...newUser._doc,
+          password: "", // không trả về mật khẩu cho người dùng
+        },
+      });
+    } else {
+      res.status(400).json({ success: false, message: "Invalid user data" });
+    }
+
     await newUser.save();
-    res.status(201).json({ success: true, message: "User registered successfully" });
   } catch (error) {
     console.log("Error on signup", error.message);
     res.status(500).json({ success: false, message: error.message });
   }
 }
 
+
+
+// hàm đăng nhập người dùng
 export async function login(req, res) {
-  res.send("login route");
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    // Kiểm tra xem người dùng có tồn tại trong cơ sở dữ liệu không
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    const ispasswordValid = await bcryptjs.compare(password, user.password);
+
+    if(!ispasswordValid){
+      return res.status(400).json({success: false, message: "Invalid credentials"});
+    }
+
+    generateTokenandsetcookie(user._id, res);
+
+    res.status(200).json({
+      success: true,
+      user: {
+        ...user._doc,
+        password: "", // không trả về mật khẩu cho người dùng
+      },
+    });
+  } catch (error) {
+    console.log("Error on login", error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
 }
 
+
+
+// hàm đăng xuất người dùng
+
 export async function logout(req, res) {
-  res.send("logout route");
+  try {
+    res.clearCookie("jwt-netflix");
+    res.status(200).json({ success: true, message: "Logged out successfully" });
+  
+  } catch (error) {
+    console.log("Error on logout", error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
 }
